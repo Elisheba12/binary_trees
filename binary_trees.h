4589f7aeba30f039b4e1a3717363eb674c6cf417
Elizabeth Salako <waleade602@gmail.com>
2022-08-24 21:09:23 -0500

    updated header file

--- binary_trees.h
+++ binary_trees.h
@@ -7,6 +7,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <limits.h>
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -53,4 +54,6 @@ int binary_tree_is_complete(const binary_tree_t *tree);
 int btic_helper(const binary_tree_t *tree, size_t index, size_t size);
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
+int btib_helper(const binary_tree_t *tree, int min, int max);
+int binary_tree_is_bst(const binary_tree_t *tree);
 #endif
